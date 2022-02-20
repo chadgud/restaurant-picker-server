f2d944b4daa1565db0fba8e7ef9e65fb46273a8c
chadgud <chad@goodyfamily.org>
2022-02-20 00:03:04 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const axios = require('axios');
+const cors = require('cors');
+
+dotenv.config({ path: './.env' });
+const app = express();
+
+const corsOptions = {
+    origin: 'https://frosty-franklin-cbd13f.netlify.app',
+    optionsSuccessStatus: 200
+}
+
+app.use('/', cors(corsOptions), (req, res, next) => {
+    const { lat, lon } = req.query;
+
+    const reqConfig = {
+        method: 'get',
+        headers: {
+            Accept: 'application.json',
+            Authorization: process.env.FOURSQUARE_API_KEY
+        },
+        params: {
+            ll: `${parseFloat(lat).toFixed(4)},${parseFloat(lon).toFixed(4)}`,
+            radius: '11265',
+            categories: '13065',
+            fields: 'name,location,categories,distance,tel,website,hours,rating,price,photos',
+            sort: 'distance',
+            limit: 25
+        }
+    };
+
+    axios.get('https://api.foursquare.com/v3/places/search', reqConfig)
+        .then((response) => {
+            res.json(response.data);
+        })
+        .catch((err) => {
+            res.send(err);
+        });
+
+});
+
+app.listen(3001, () => {
+    console.log('App Listening on port 3001');
+});
